09eba4e129c5eccd0a1ab358a55d263933c21abf
rddv <rddv@list.ru>
2015-12-24 16:49:37 +0200

    All

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 
-$recepient = "youmail@ya.ru";
-$sitename = "Название сайта";
+$recepient = "rddv@mail.ru";
+$sitename = "moDesign";
 
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
+
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone;
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
